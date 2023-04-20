2961190b4a37ea00660431f2fadd675f2bfe22ca
Dawid Chruściński <35899656+dchruscinski@users.noreply.github.com>
2023-04-20 22:53:43 +0200

    Added additional test thing

--- src/main/java/pl/dchruscinski/controller/HealthCheckController.java
+++ src/main/java/pl/dchruscinski/controller/HealthCheckController.java
@@ -4,10 +4,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/healthcheck")
@@ -26,6 +32,24 @@ public class HealthCheckController {
         } else if (format.equalsIgnoreCase(DETAILED_FORMAT)) {
             logger.debug("getHealthCheck(): returning detailed healthcheck.");
             response.sendRedirect(INFO_REDIRECT);
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @GetMapping(path = "/message", produces = MediaType.APPLICATION_JSON_VALUE)
+    // @GetMapping(path = "/message", produces = "application/json")
+    public ResponseEntity<?> getCustomJsonMessage(@RequestParam String format) {
+        if (format.equalsIgnoreCase(SIMPLE_FORMAT)) {
+            logger.debug("getCustomJsonMessage(): returning simple message.");
+            return ResponseEntity.ok(Collections.singletonMap("status", "OK"));
+
+        } else if (format.equalsIgnoreCase(DETAILED_FORMAT)) {
+            Map<String, String> jsonMessageMap = new HashMap<>();
+            jsonMessageMap.put("currentTime", LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+            jsonMessageMap.put("status", "OK");
+            logger.debug("getCustomJsonMessage(): returning detailed message.");
+            return ResponseEntity.ok(jsonMessageMap);
         }
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
